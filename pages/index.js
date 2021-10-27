57fa8dc4a3b18e930a33ee3a6f3544738158afea
echizenyayota <virtualmisso30@gmail.com>
2021-10-27 15:28:25 +0900

    create the form

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { Heading, Page } from "@shopify/polaris";
+import { Button, Card, Heading, Page, Stack, TextField } from "@shopify/polaris";
 
 const Index = () => {
 
   const [appendToTitle, setAppendToTitle] = useState('');
   const [appendToDescription, setAppendToDescription] = useState('');
-  const [appendPrice, setAppendPrice] = useState('');
+  const [appendToPrice, setAppendPrice] = useState('');
   const [pickerOpen, setPickerOpen] = useState(false);
   const [products, setProducts] = useState([]);
   const [showToast, setShowToast] = useState(false);
@@ -13,6 +13,29 @@ const Index = () => {
   return (
     <Page>
       <Heading>Product Updater App</Heading>
+      <Card>
+        <Card.Section>
+          <Stack vertical>
+            <TextField
+              label="Append to title"
+              value={appendToTitle}
+              onChange={setAppendToTitle}
+            />
+            <TextField
+              label="Append to description"
+              value={appendToDescription}
+              onChange={setAppendToDescription}
+              multiline={3}
+            />
+            <TextField
+              label="Append to title"
+              value={appendToPrice}
+              onChange={setAppendPrice}
+            />
+            <Button primary onClick={ () => console.log('Clicked')}>Select Products</Button>
+          </Stack>
+        </Card.Section>
+      </Card>
     </Page>
   );
 
