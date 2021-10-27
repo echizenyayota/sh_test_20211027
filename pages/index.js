9a5e6512004d6331c13087036badfee3abd3569a
echizenyayota <virtualmisso30@gmail.com>
2021-10-27 15:16:27 +0900

    Define the hooks

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import { Heading, Page } from "@shopify/polaris";
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+const Index = () => {
 
-export default Index;
+  const [appendToTitle, setAppendToTitle] = useState('');
+  const [appendToDescription, setAppendToDescription] = useState('');
+  const [appendPrice, setAppendPrice] = useState('');
+  const [pickerOpen, setPickerOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [showToast, setShowToast] = useState(false);
+
+  return (
+    <Page>
+      <Heading>Product Updater App</Heading>
+    </Page>
+  );
+
+};
+
+
+export default Index; 
