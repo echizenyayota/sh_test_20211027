2e7c2d5821001163374a8cbe0d5076f87585f52f
echizenyayota <virtualmisso30@gmail.com>
2021-10-29 11:04:20 +0900

    Add the Toast

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import { useState, useMemo } from "react";
-import { Button, Card, DataTable, EmptyState, Heading, Page, Stack, TextField } from "@shopify/polaris";
+import { useState, useMemo, useCallback } from "react";
+import { Button, Card, DataTable, EmptyState, Frame, Heading, Page, Stack, Toast, TextField } from "@shopify/polaris";
 import { ResourcePicker } from "@shopify/app-bridge-react";
 
 
@@ -18,52 +18,70 @@ const Index = () => {
     `${product.title}${appendToTitle}`,
     product.descriptionHtml,
     `${product.descriptionHtml}${appendToDescription}`,
-    product.variants[0].price
+    product.variants[0].price,
   ]), [products, appendToTitle, appendToDescription, appendToPrice]);
 
+  const submitHandler = useCallback(() => {
+    console.log('SubmitHandler');
+    setShowToast(true);
+  }, []);
+
+  const toastMarkup = showToast ? 
+    <Toast
+      content="Update Successful"
+      onDismiss={() => setShowToast(false)}
+      duration={4000}
+    /> : null;
+
   return (
-    <Page>
-      <Heading>Product Updater App</Heading>
-      <Card>
-        <Card.Section>
-          <Stack vertical>
-            <TextField
-              label="Append to title"
-              value={appendToTitle}
-              onChange={setAppendToTitle}
-            />
-            <TextField
-              label="Append to description"
-              value={appendToDescription}
-              onChange={setAppendToDescription}
-              multiline={3}
-            />
-            <TextField
-              label="Append to title"
-              value={appendToPrice}
-              onChange={setAppendPrice}
-            />
-            <ResourcePicker
-              resourceType="Product"
-              showVariants={false}
-              open={pickerOpen}
-              onSelection={ (resources) => {
-                console.log(resources);
-                setProducts(resources.selection);
-              }}
-            />
-            <Button primary onClick={() => setPickerOpen(true)}>Select Products</Button>
-          </Stack>
-        </Card.Section>
-        <Card.Section>
-          {productTableDisplayData.length ? <DataTable
-            columnContentTypes={['text', 'text', 'text', 'text', 'text', 'numeric', 'numeric']}
-            headings={['ID', 'Old Title', 'NewTitle', 'Old Description', 'New Description','Old Price', 'New Price']}
-            rows={productTableDisplayData}
-          /> : <EmptyState heading="No products selection"/>}
-        </Card.Section>
-      </Card>
-    </Page>
+    <Frame>
+      <Page>
+        <Heading>Product Updater App</Heading>
+        <Card>
+          <Card.Section>
+            <Stack vertical>
+              <TextField
+                label="Append to title"
+                value={appendToTitle}
+                onChange={setAppendToTitle}
+              />
+              <TextField
+                label="Append to description"
+                value={appendToDescription}
+                onChange={setAppendToDescription}
+                multiline={3}
+              />
+              <TextField
+                label="Append to title"
+                value={appendToPrice}
+                onChange={setAppendPrice}
+              />
+              <ResourcePicker
+                resourceType="Product"
+                showVariants={false}
+                open={pickerOpen}
+                onSelection={ (resources) => {
+                  console.log(resources);
+                  setProducts(resources.selection);
+                }}
+              />
+              <Button primary onClick={() => setPickerOpen(true)}>Select Products</Button>
+            </Stack>
+          </Card.Section>
+          <Card.Section>
+            {productTableDisplayData.length ? <DataTable
+              columnContentTypes={['text', 'text', 'text', 'text', 'text', 'numeric', 'numeric']}
+              headings={['ID', 'Old Title', 'NewTitle', 'Old Description', 'New Description','Old Price', 'New Price']}
+              rows={productTableDisplayData}
+            /> : <EmptyState heading="No products selection"/>}
+          </Card.Section>
+          <Card.Section>
+            <Button primary onClick={submitHandler} disabled={!products.length}>Submit</Button>
+          </Card.Section>
+        </Card>
+        {toastMarkup}
+      </Page>
+    </Frame>
   );
 
 };
